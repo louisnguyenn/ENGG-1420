b9d14aa83ccef578a33b3479d44bf0f179a5a6fb
Louis Nguyen <louis.nguyen550@gmail.com>
2026-02-02 20:34:25 -0500

    add A2 question 4

---
+++ Assignments/A2Q4.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class A2Q4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = -1;
+        int sum = 0;
+        
+        System.out.println("Enter numbers (0 to exit):");
+        while (num != 0)
+        {
+            if (num == 0)
+            {
+                break;
+            }
+
+            num = scanner.nextInt();
+
+            sum += num;
+        }
+        System.out.println("Sum: " + sum);
+
+        scanner.close();
+    }
+}
