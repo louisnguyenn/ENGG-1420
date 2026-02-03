d3dd74db565e5295c18820746cadf79551d98c0e
Louis Nguyen <louis.nguyen550@gmail.com>
2026-02-02 20:27:09 -0500

    add A2Q3

---
+++ Assignments/A2Q3.java
@@ -0,0 +1,18 @@
+package Assignments;
+
+public class A2Q3 {
+    public static void main(String[] args) {
+        int y = 0;
+        int x = 0;
+        if (y == 8 && x == 5)
+        {
+            System.out.println("@@@@@");
+        }
+        else
+        {
+            System.out.println("#####");
+            System.out.println("$$$$$");
+            System.out.println("&&&&&");
+        }
+    }
+}
