ff49f56c5b1ecd3e0c6f48ed4a1349715e9e189a
Louis Nguyen <louis.nguyen550@gmail.com>
2026-03-16 19:51:40 -0400

    add lab 8 - testing with junit

---
+++ Labs/Lab 8/test/java/AlphabetTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AlphabetTest {
+    private String s = "supercalifragilisticexpialidocious!~";
+
+    @Test
+    public void testCountVowels() throws Exception {
+        int vowels = Alphabet.countVowels(s);
+        assertEquals(16, vowels);
+    }
+
+    @Test
+    public void testCountConsonants() throws Exception {
+        int consonants = Alphabet.countConsonants(s);
+        assertEquals(18, consonants);
+    }
+}
