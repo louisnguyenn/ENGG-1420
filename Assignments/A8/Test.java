8f015a1b98ac1d69ab3e458defa5c20bec07f9d5
Louis Nguyen <louis.nguyen550@gmail.com>
2026-03-19 09:08:03 -0400

    implement test for A8

--- Assignments/A8/Test.java
+++ Assignments/A8/Test.java
@@ -1,2 +1,12 @@
 public class Test {
+    public static void main(String[] args) {
+        Student[] students = {new Student("Louis", "Nguyen", 147831, 3.90), new Student("John",
+                "Doe", 231798, 3.72)};
+
+        System.out.println(students[0].compareTo(students[1]));
+
+        Circle[] circles = {new Circle(5), new Circle(5)};
+
+        System.out.println(circles[0].compareTo(circles[1]));
+    }
 }
